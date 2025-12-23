8d64cad68dd9fc586f515e26abafa2f6b171d404
kingstonebridge2026 <jamalassker2032@gmail.com>
2025-12-23 11:45:08 +0300

    Update Binance API keys and Telegram chat ID

--- hft_trader.py
+++ hft_trader.py
@@ -7,10 +7,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # ===== CONFIGURATION =====
-BINANCE_API_KEY = os.getenv("BINANCE_KEY", "ZWHzZmOXxWwB6Qo2PXM5oiC799JidzTsXiLkcIVTWJLceMxjq3Qy0xPbph229M3Q")
-BINANCE_SECRET = os.getenv("BINANCE_SECRET", "QacEqE0bSlOvZitUSnybJaBTYalrRAG2nslB4aYCJraOtCsPUr2fQUxB5e0TsntL")
+BINANCE_API_KEY = os.getenv("BINANCE_KEY", "UuTdZGp7331MhmnoukbkDW3VtF6Z9hnHMG3b75dWavAlG9e1zNbv2lBrjHkMqMpl")
+BINANCE_SECRET = os.getenv("BINANCE_SECRET", "6Rs8ef3mZarvB8I6J2ewSCZwYmmohyyUVE4020AzsqzGoQt6WNXdTwqN1jZwh2i0")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN", "8287625785:AAEr1IXBXadMg20hehUrwBoMEYaBBOY4OMU")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "5665906172")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "93372553")
 
 SYMBOL = "BTC/USDT"
 POSITION_SIZE = 0.001 
